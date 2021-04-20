dbece90f3dc9009ede4d69f7ca5bccc75b310d87
sengmitnick <sengmitnick@163.com>
2021-04-21 04:18:40 +0800

    COMMIT

--- examples/taro-react/babel-plugin-auto-import.js
+++ examples/taro-react/babel-plugin-auto-import.js
@@ -4,24 +4,25 @@ module.exports = function ({ types }) {
   return {
     visitor: {
       ExportDefaultDeclaration: {
-        enter(path, source) {
+        exit(path, source) {
           const { filename, opts: { entry } } = source;
           if (!entry.includes(filename)) return;
           const declaration = t.identifier(path.node.declaration.name)
-          const newNode = t.exportDefaultDeclaration(
-            t.callExpression(
-              t.memberExpression(
-                t.callExpression(
-                  t.identifier('require'),
-                  [t.stringLiteral('@/components/Container')]
-                ), t.identifier('default')
-              ),
-              [declaration]
-            ))
-            // const newNode = t.exportDefaultDeclaration(declaration)
-          // console.log(JSON.stringify(path.node));
+          // const newNode = t.exportDefaultDeclaration(
+          //   t.callExpression(
+          //     t.memberExpression(
+          //       t.callExpression(
+          //         t.identifier('require'),
+          //         [t.stringLiteral('@/components/Container')]
+          //       ), t.identifier('default')
+          //     ),
+          //     [declaration]
+          //   ))
+          const newNode = t.exportDefaultDeclaration(declaration)
+          console.log(JSON.stringify(newNode));
           // console.log(path.parentPath, newNode);
-          path.replaceWith(newNode)
+          path.replaceWith(newNode);
+          path.skip();
           // path.node = newNode;
         }
 
